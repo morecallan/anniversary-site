d51d1808b63e9b9dea38b1ea03197ed98a43a9d9
Callan Morrison <callan.l.morrison@gmail.com>
2017-04-09 22:15:44 -0500

    menu configuration

--- public/app/app.config.js
+++ public/app/app.config.js
@@ -30,3 +30,13 @@ app.constant("FIREBASE_CONFIG", {
 	storageBucket: "anniversary-61e42.appspot.com",
 	messagingSenderId: "890352153892"
 })
+
+app.config(function(){
+	$('.button-collapse').sideNav({
+		 menuWidth: 160, // Default is 300
+		 edge: 'left', // Choose the horizontal origin
+		 closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+		 draggable: true // Choose whether you can drag to open on touch screens
+	 }
+ );
+})
